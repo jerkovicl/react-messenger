858dbbedc58289cba91fc56ba0aea388e4eefdc0
Luka Jerkovic <lukajerkovic@protonmail.com>
2019-05-23 23:02:35 +0200

    init

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import AppNavbar from './AppNavbar';
-import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
+//import AppNavbar from './AppNavbar';
+import JobsList from './JobsList';
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <AppNavbar />
-                <Container fluid>
-                    <Button color="link"><Link to="/jobs">Manage Schedule Jobs</Link></Button>
-                </Container>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <JobsList />
+      </div>
+    );
+  }
 }
 
 export default Home;
